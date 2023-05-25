0daf7fd30d8d8c77948f4e300b0122846cf50831
Timothy Batt <timothybatt3@gmail.com>
2023-05-25 16:40:09 -0400

    added upload folder button

--- react/src/components/UploadForm.js
+++ react/src/components/UploadForm.js
@@ -29,8 +29,8 @@ export function UploadButton() {
         <input 
             className='hidden' 
             type="file" 
-            name="files[]" 
-            id="file" multiple
+            name='files[]'
+            id='file' multiple
             onChange={ uploadFile } 
         />
         <button 
@@ -43,10 +43,52 @@ export function UploadButton() {
     );
 }
 
-export default function UploadForm(props) {
+export function UploadFolderButton() {
+    const [error, setError] = useState(null);
+
+    function uploadFile(e) {
+        e.preventDefault();
+        const uploadForm = document.querySelector('#uploadForm');
+        axios.post(uploadPath, new FormData(uploadForm))
+        .then(_ => window.location.reload())
+        .catch(err => {
+            console.error(err)
+            setError(err.message);
+        });
+    }
+
+    function handleUploadClick(e) {
+        e.preventDefault();
+        document.querySelector(`#folder`).click()
+    }
+
     return (
-    <form id='uploadForm' action={ uploadPath } method='POST' encType="multipart/form-data" className='relative'>
+    <>
+        <input 
+            className='hidden' 
+            type='file'
+            name='folder'
+            id='folder'
+            mozdirectory='true'
+            webkitdirectory='true'
+            onChange={ uploadFile } 
+        />
+        <button 
+        className='cursor-pointer rounded border bg-slate-50 hover:bg-slate-100 py-2 px-2' 
+        onClick={ handleUploadClick }
+        >Folder Upload</button>
+
+        <Error>{ error }</Error>
+    </>
+    );
+}
+
+
+export default function UploadForm() {
+    return (
+    <form id='uploadForm' action={ uploadPath } method='POST' encType="multipart/form-data" className='relative grid gap-4'>
         <UploadButton/>
+        <UploadFolderButton/>
     </form>
     );
 }
